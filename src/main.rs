57672b84a8743acd5536bd197e358e71afd55f4b
Philipp <philipp.thiel@inf.ethz.ch>
2022-10-19 19:55:40 +0200

    offically too dumb for traits

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,41 @@
-trait Exp {
-    fn replace_var(self, var: char, new_exp: Box<&dyn Exp>) -> Box<dyn Exp>;
+#[derive(Clone)]
+enum Exp {
+    Lambda(Lambda),
+    Var(char),
+    Const(i32),
 }
 
+impl Exp {
+    fn replace_var(self, var: char, new_exp: Box<&Exp>) -> Box<Exp> {
+        match self {
+            Exp::Lambda(l) => l.replace_var(var, new_exp),
+            Exp::Var(v) if v == var => Box::new((**new_exp).clone()),
+            _ => Box::new(self),
+        }
+    }
+}
+
+#[derive(Clone)]
 struct Lambda {
     arg: char,
-    exp: Box<dyn Exp>,
+    exp: Box<Exp>,
 }
 
-impl Exp for Lambda {
-    fn replace_var(self, var: char, new_exp: Box<&dyn Exp>) -> Box<dyn Exp> {
+impl Lambda {
+    fn replace_var(self, var: char, new_exp: Box<&Exp>) -> Box<Exp> {
         if var == self.arg {
-            return Box::new(self);
+            return Box::new(Exp::Lambda(self));
         }
 
-        return Box::new(Self {
+        return Box::new(Exp::Lambda(Self {
             arg: self.arg,
             exp: self.exp.replace_var(var, new_exp),
-        });
+        }));
     }
 }
 
 impl Lambda {
-    fn new(arg: char, exp: Box<dyn Exp>) -> Self {
+    fn new(arg: char, exp: Box<Exp>) -> Self {
         Self { arg, exp }
     }
 }
