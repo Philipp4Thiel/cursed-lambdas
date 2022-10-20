352434ed9cde85956f8a4ae93a5525acfc3d4e1a
Philipp <philipp.thiel@inf.ethz.ch>
2022-10-20 12:52:03 +0200

    stuff

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,41 @@ enum Exp {
     Lambda(Lambda),
     Var(char),
     Const(i32),
+    Op(Operation),
 }
 
 impl Exp {
-    fn replace_var(self, var: char, new_exp: Box<&Exp>) -> Box<Exp> {
+    fn replace_var(self, var: char, new_exp: &Box<Exp>) -> Box<Exp> {
         match self {
             Exp::Lambda(l) => l.replace_var(var, new_exp),
-            Exp::Var(v) if v == var => Box::new((**new_exp).clone()),
+            Exp::Var(v) if v == var => new_exp.clone(),
+            Exp::Op(op) => op.replace_var(var, new_exp),
             _ => Box::new(self),
         }
+    }
+}
+
+#[derive(Clone, Copy)]
+enum Operator {
+    Add,
+    Sub,
+    Mul,
+}
+
+#[derive(Clone)]
+struct Operation {
+    op: Operator,
+    e1: Box<Exp>,
+    e2: Box<Exp>,
+}
+
+impl Operation {
+    fn replace_var(self, var: char, new_exp: &Box<Exp>) -> Box<Exp> {
+        Box::new(Exp::Op(Self {
+            op: self.op,
+            e1: self.e1.replace_var(var, new_exp),
+            e2: self.e2.replace_var(var, new_exp),
+        }))
     }
 }
 
@@ -22,7 +48,7 @@ struct Lambda {
 }
 
 impl Lambda {
-    fn replace_var(self, var: char, new_exp: Box<&Exp>) -> Box<Exp> {
+    fn replace_var(self, var: char, new_exp: &Box<Exp>) -> Box<Exp> {
         if var == self.arg {
             return Box::new(Exp::Lambda(self));
         }
